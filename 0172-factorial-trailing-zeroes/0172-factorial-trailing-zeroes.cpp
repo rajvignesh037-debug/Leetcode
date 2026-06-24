3ade0327aaa98f2d37b4b46fbf888fa0031e4394
rajvignesh037-debug <rajvignesh037@gmail.com>
2026-06-24 20:39:01 +0530

    Time: 0 ms (100%), Space: 8 MB (86.24%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count=0;
+        for(long long i=5;i<=n;i*=5){
+            count+=n/i;
+        }
+        return count;
+    }
+};
